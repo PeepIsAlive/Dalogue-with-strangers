2efa6af5d310a9c3ac3939034b4274793ac103b3
PeepIsAlive <87329383+PeepIsAlive@users.noreply.github.com>
2023-07-09 20:46:46 +0700

    input field on main screen

--- Assets/Scripts/Starters/StarterMain.cs
+++ Assets/Scripts/Starters/StarterMain.cs
@@ -8,14 +8,21 @@ namespace Starters
 {
     public class StarterMain : Starter, ISceneLoadHandler<string>
     {
-        [SerializeField] private Image _background;
+        [SerializeField] private Image _sceneBackground;
+
         private string _npcPresetId;
+
+        public override void OnSceneLoad(string argument)
+        {
+            _npcPresetId = argument;
+        }
 
         protected override async Task Initialize()
         {
             var npcPreset = SettingsProvider.Get<NpcCommonSettings>().GetPreset(_npcPresetId);
 
-            _background.sprite = npcPreset.Background;
+            _sceneBackground.sprite = npcPreset.Background;
+
             npcPreset.Prefabs.ForEach(p =>
             {
                 Instantiate(p);
@@ -28,11 +35,6 @@ namespace Starters
         {
             LoadSceneProcessor.Instance.InvokeLoadAction();
             await Initialize();
-        }
-
-        public override void OnSceneLoad(string argument)
-        {
-            _npcPresetId = argument;
         }
     }
 }
