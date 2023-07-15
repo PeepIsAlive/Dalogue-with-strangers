f7ca8f50437d5ce446a929b59d538cb40b75bd73
PeepIsAlive <87329383+PeepIsAlive@users.noreply.github.com>
2023-07-16 03:41:00 +0700

    popup display system

---
+++ Assets/Scripts/UI/Popups/DefaultPopupView.cs
@@ -0,0 +1,10 @@
+namespace UI
+{
+    public sealed class DefaultPopupView : PopupView<DefaultPopup>
+    {
+        new public void Setup(DefaultPopup settings)
+        {
+            base.Setup(settings);
+        }
+    }
+}
