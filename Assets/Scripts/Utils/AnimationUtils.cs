e0acc929ba669282af7aa17b0fa8cc2f58e10f0e
PeepIsAlive <87329383+PeepIsAlive@users.noreply.github.com>
2023-08-12 19:37:15 +0700

    animation utils

---
+++ Assets/Scripts/Utils/AnimationUtils.cs
@@ -0,0 +1,21 @@
+using Core.Animations;
+
+namespace Utils
+{
+    public static class AnimationUtils
+    {
+        public static string GetAnimationName(NatoriAnimationType type)
+        {
+            return type.ToString();
+        }
+
+        public static string GetAnimationName(EpsilonAnimationType type)
+        {
+            return type switch
+            {
+                EpsilonAnimationType.ToBeEmbarrassedAndShakeHead => "ToBeEmbarrassed&ShakeHead",
+                _ => type.ToString()
+            };
+        }
+    }
+}
