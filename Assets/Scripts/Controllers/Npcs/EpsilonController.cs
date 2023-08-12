98c4fac296edc20e3784c85c8ab6a6db4fa36e50
PeepIsAlive <87329383+PeepIsAlive@users.noreply.github.com>
2023-08-12 19:43:06 +0700

    промежуточный

--- Assets/Scripts/Controllers/Npcs/EpsilonController.cs
+++ Assets/Scripts/Controllers/Npcs/EpsilonController.cs
@@ -1,12 +1,16 @@
 using Core.Animations;
+using UnityEngine;
+using Utils;
 
 namespace Controllers
 {
     public sealed class EpsilonController : NpcController, IAnimable<EpsilonAnimationType>
     {
+        [SerializeField] private Animator _animator;
+
         public void PlayAnimation(EpsilonAnimationType type)
         {
-
+            _animator.SetTrigger(AnimationUtils.GetAnimationName(type));
         }
     }
 }
--- Assets/Scripts/Controllers/Npcs/NatoriController.cs
+++ Assets/Scripts/Controllers/Npcs/NatoriController.cs
@@ -1,12 +1,16 @@
 using Core.Animations;
+using UnityEngine;
+using Utils;
 
 namespace Controllers
 {
     public sealed class NatoriController : NpcController, IAnimable<NatoriAnimationType>
     {
+        [SerializeField] private Animator _animator;
+
         public void PlayAnimation(NatoriAnimationType type)
         {
-
+            _animator.SetTrigger(AnimationUtils.GetAnimationName(type));
         }
     }
 }
